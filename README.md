36f0ae9fcbc2dd1cc7c37bff7cc67071f6768617
Samuel Loza <zamuel.loza@gmail.com>
2021-06-23 20:07:41 -0400

    Add value types

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # BootCamp04-backend
 
-1.
+1. Fibonacci
 1. Variables and Declaration  
 ![title](screenshots/variableDeclaration.png)
+1. Nullables
+![title](screenshots/nullables.png)
+1. Value Types  
+![title](screenshots/valueTypes.png)
---
+++ problems/valueTypes/valueTypes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace valueTypes
+{
+    public struct DetaildInteger 
+    {
+
+        public List<string> detail;
+        public int Number;
+        public DetaildInteger(int number)
+        {
+            detail = new List<string>();
+            this.Number = number;
+        }
+    
+        public void AddDetail(string detail)
+        {
+            this.detail.Add(detail);
+        }
+ 
+        public override string ToString()
+        {
+            return this.Number + " [" + string.Join(",", detail.ToArray()) + "]";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n1 = new DetaildInteger(0);
+            n1.AddDetail("A");
+            Console.WriteLine(n1);
+
+            var n2 = n1;
+            n2.Number = 7;
+            n2.AddDetail("B");
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+        }
+    }
+}
