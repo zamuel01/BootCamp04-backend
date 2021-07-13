10080f4c8ff835f252ea64ff07592921ba383692
Samuel Loza <starsaminf@gmail.com>
2021-07-13 12:48:47 -0400

    add exampleGenericContraints

---
+++ problems/exampleGenericContraints/exampleGenericContraints/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace exampleGenericContraints
+{
+    class MyClass
+    {
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
