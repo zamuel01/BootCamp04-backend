7f845171b2dc9310c684087c8bfd93d4b2ba81b1
Samuel Loza <starsaminf@gmail.com>
2021-07-13 12:47:50 -0400

    Add anonymous

---
+++ problems/anonymous/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace anonymous
+{
+    class Program
+    {
+        public delegate void Print(int value);
+        static void Main(string[] args)
+        {
+            Print print = delegate (int val)
+            {
+                Console.WriteLine("inside anonymous Method :{0}", val);
+            };
+
+            print(100);
+        }
+    }
+}
